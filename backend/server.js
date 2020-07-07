b9549e0819dd075392bbbf7915d00a0ad8283d52
Michard Maxime <maxime.michard@laposte.net>
2020-07-07 17:23:18 +0200

    commentaire server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const http = require('http');
-const app = require('./app');
+const http = require('http'); // Importation package HTTP //
+const app = require('./app'); // Importation du fichier app.js//
 
-const normalizePort = val => {
+const normalizePort = val => {  // Renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaine //
   const port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -12,10 +12,10 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000'); // Port écouter : 3000 (port par défaut)//
 app.set('port', port);
 
-const errorHandler = error => {
+const errorHandler = error => { // Recherche les différentes erreurs et les gère de manière appropriée. Ensuite enregistrée dans le serveur //
   if (error.syscall !== 'listen') {
     throw error;
   }
@@ -35,13 +35,13 @@ const errorHandler = error => {
   }
 };
 
-const server = http.createServer(app);
+const server = http.createServer(app); // Création serveur (use app) //
 
-server.on('error', errorHandler);
+server.on('error', errorHandler);  // Ecouteur d'événement enregistré, Consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console // 
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
 });
 
-server.listen(port);
+server.listen(port); // Serveur listen port : 3000 //
