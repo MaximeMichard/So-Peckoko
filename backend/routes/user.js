9fe416b18dcae939031947884fb8787fed317b84
Michard Maxime <maxime.michard@laposte.net>
2020-08-03 15:25:34 +0200

    frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express= require ('express'); // Utilisation framework basé sur node.JS //
 const router= express.Router(); 
 const userCtrl= require ('../controllers/user');
-const expressBouncer= require ("express-bouncer")(500,10000,3); // Contre les attaques de force Brute // 
+const expressBouncer= require ("express-bouncer")(5000,600000,3); // Contre les attaques de force Brute // 
 
 router.post('/signup', userCtrl.signup);
 router.post('/login',expressBouncer.block, userCtrl.login);
